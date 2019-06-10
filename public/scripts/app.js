860774492592360c989eb293748a16ad99888a97
Javier Moraga Rojas <jmoraga@Javier.local>
2019-06-11 01:31:20 +1000

    NEW - Some tests with babel-env and babel-react. Also playing with JSX Syntax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,61 @@
-"use strict";
+'use strict';
 
 console.log("Holi");
 
 // JavaScript + XML = JSX
+var info = {
+    title: 'Indecision App',
+    subtittle: 'App for indecision'
+};
 var template = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-        "h1",
+        'h1',
         null,
-        "Indecision App!"
+        info.title
     ),
     React.createElement(
-        "p",
+        'p',
         null,
-        "This is some text"
+        info.subtittle
+    )
+);
+var user = {
+    name: 'Javier',
+    age: 29,
+    location: 'Hobart'
+};
+
+function getLocation(location) {
+    if (location) {
+        return location;
+    } else {
+        return 'Unknown';
+    }
+};
+
+var template_two = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        getLocation(user.location)
     )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template_two, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
 console.log("Holi");
 
 // JavaScript + XML = JSX
+var info = {
+    title: 'Indecision App',
+    subtittle: 'App for indecision'
+};
 var template = (
     <div>
-        <h1>Indecision App!</h1>
-        <p>This is some text</p>
+        <h1>{info.title}</h1>
+        <p>{info.subtittle}</p>
+    </div>
+);
+var user = {
+    name: 'Javier',
+    age: 29,
+    location: 'Hobart'
+};
+
+function getLocation(location){
+    if(location){
+        return location;
+    }
+    else{
+        return 'Unknown';
+    }
+};
+
+var template_two = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        <p>Location: {getLocation(user.location)}</p>
     </div>
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template_two, appRoot);
